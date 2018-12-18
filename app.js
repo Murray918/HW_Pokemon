0ef70dfcdeea2abe6d4070cd7080c579cccb1cfd
Tristan Bennett <tristanbennett@tristanova.local>
2018-12-18 14:18:56 -0600

    created app.js file, added some variables

---
+++ app.js
@@ -0,0 +1,41 @@
+let deck = [
+  { name: "Bulbasaur", damage: 60 }, 
+  { name: "Caterpie", damage: 40 }, 
+  { name: "Charmander", damage: 60 }, 
+  { name: "Clefairy", damage: 50 }, 
+  { name: "Jigglypuff", damage: 60 }, 
+  { name: "Mankey", damage: 30 }, 
+  { name: "Meowth", damage: 60 }, 
+  { name: "Nidoran - female", damage: 60 }, 
+  { name: "Nidoran - male", damage: 50 }, 
+  { name: "Oddish", damage: 40 }, 
+  { name: "Pidgey", damage: 50 }, 
+  { name: "Pikachu", damage: 50 }, 
+  { name: "Poliwag", damage: 50 }, 
+  { name: "Psyduck", damage: 60 }, 
+  { name: "Rattata", damage: 30 }, 
+  { name: "Squirtle", damage: 60 }, 
+  { name: "Vulpix", damage: 50 }, 
+  { name: "Weedle",  damage: 40 }
+]
+
+let player = {
+	hand: [
+		{card1},
+		{card2},
+		{card3}
+			],
+	points: 0
+}
+
+let arena = [
+	{playerOne.card},
+	{playerTwo.card}
+]
+
+playerOne.hand.push(three random cards)
+playerTwo.hand.push(three random cards)
+
+Math.random()*
+
+
