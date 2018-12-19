abcb793aed97b27e33b96f5cf4de976c09a3c64c
Tristan Bennett <tristanbennett@tristanova.local>
2018-12-18 18:50:25 -0600

    added logic for player twos random card draw, started logic for player one card draw

--- app.js
+++ app.js
@@ -21,37 +21,49 @@ let deck = [
 //create objects for each player
 let playerOne = {
 	hand: [],
-	points: 0,
+	points: 0
 }
 
 let playerTwo = {
   hand: [],
-  points: 0,
+  points: 0
 }
 
 //create arena to hold two players' card being played against each other
-let arena = []
+let arena = [];
 
 //code for one round
-
+console.log('Round 1 Start!');
 // Remove random card from deck array and push it into playerOne.hand array
 for (let i = 0; i <= 2; i++) {
   playerOne.hand.push(deck.splice(Math.floor(Math.random() * deck.length), 1));
 }
+console.log(`Player One drew ${playerOne.hand[0][0].name}, ${playerOne.hand[1][0].name}, and ${playerOne.hand[2][0].name}!`);
+
 // Remove random card from deck array and push it into playerTwo.hand array
 for (let i = 0; i <= 2; i++) {
   playerTwo.hand.push(deck.splice(Math.floor(Math.random() * deck.length), 1));
 }
-
+console.log(`Player Two drew ${playerTwo.hand[0][0].name}, ${playerTwo.hand[1][0].name}, and ${playerTwo.hand[2][0].name}!`);
 //code for one turn
 
 //play hands
+//playerOne picks card
+function compareNumbers(a, b) {
+  return a - b;
+}
 
-//compare cards
 
+//playerTwo picks card
+arena.push(playerTwo.hand.splice(Math.floor(Math.random() * playerTwo.hand.length), 1));
+console.log(arena[0])
+// console.log(arena);
 
-
-
+// console.log the round number
+// console.log the players hand
+// console.log the chosen card
+// console.log who won the turn
+// console.log who won the round
 
 
 
