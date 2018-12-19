cd8cac2a27af5884ed43636bc128b58aaf2f1bcd
ohheyitslance <ohheyitslance@ohheyitslances-MacBook.local>
2018-12-19 17:35:40 -0600

    added several pieces of code for who won the round logic for how to compare who had the highest card each round amongst other pieces

--- pokemongame.js
+++ pokemongame.js
@@ -18,39 +18,79 @@ let deck = [
   { name: "Vulpix", damage: 50 }, 
   { name: "Weedle",  damage: 40 }
 ]
+
 // player one's hand, and his points
 let playerOne = {
 	hand: [],
-	points:0
+	turnPoints: 0,
+	roundsWon: 0
 }
 // player two's hand, and his points
 let playerTwo = {
 	hand: [],
-	points:0 
+	turnPoints: 0, 
+	roundsWon: 0
 }
 
 // arena to house each players' card currently being played
 let arena = []
 
-// code for 1 round
+// console.log the round number
 console.log("Round 1, Start!")
+
+
+// console.log the players hand
 // for loop to randomize the cards in the deck, and splice 3 cards into player one's hand
 for (let i = 0; i <= 2; i++){
-	playerOne.hand.push(deck.splice(Math.floor(Math.random() * deck.length), 1));
+	playerOne.hand.push(deck.splice(Math.floor(Math.random() * deck.length), 1)[0]);
 }
+let highestCard = {};
+// console.log(playerOne.hand[0][0].damage, playerOne.hand[1][0].damage)
+if (playerOne.hand[0].damage >= playerOne.hand[1].damage){
+	if (playerOne.hand[0].damage >= playerOne.hand[2].damage){
+		highestCard = playerOne.hand[0]
+	} else {
+		highestCard = playerOne.hand[2]
+	}
+}
+else if (playerOne.hand[0].damage < playerOne.hand[1].damage){
+	if (playerOne.hand[1].damage > playerOne.hand[2].damage){
+		highestCard = playerOne.hand[1]
+	} else {
+		highestCard = playerOne.hand[2]
+	}
+}
+arena.push(highestCard);
+
+
 // edit output of console.log so that console provides which cards were drawn, rather than [object Objects]
-console.log(`Player One drew ${playerOne.hand[0][0].name}, ${playerOne.hand[1][0].name}, ${playerOne.hand[2][0].name}!`)
+console.log(`Player One drew ${playerOne.hand[0].name}, ${playerOne.hand[1].name}, ${playerOne.hand[2].name}!`)
 // for loop to randomize the cards in the deck, and splice 3 cards into player two's hand
 for (let i = 0; i <= 2; i++){
-	playerTwo.hand.push(deck.splice(Math.floor(Math.random() * deck.length), 1));
+	playerTwo.hand.push(deck.splice(Math.floor(Math.random() * deck.length), 1)[0]);
 }
 // output for the hand that player two drew
-console.log(`Player Two drew ${playerTwo.hand[0][0].name}, ${playerTwo.hand[1][0].name}, ${playerTwo.hand[2][0].name}!`)
+console.log(`Player Two drew ${playerTwo.hand[0].name}, ${playerTwo.hand[1].name}, ${playerTwo.hand[2].name}!`)
 // used to randomly push a card from player two's hand into the arena
-arena.push(playerTwo.hand.splice(Math.floor(Math.random() * playerTwo.hand.length), 1));
-	 console.log(arena)
-// console.log the round number
-// console.log the players hand
+arena.push(playerTwo.hand.splice(Math.floor(Math.random() * playerTwo.hand.length), 1)[0]);
 //	console.log the chosen card
+	
+	 
+// who played which cards
+console.log(arena[0].name + " vs. " + arena[1].name)
+
 // 	console.log who won the turn
+if (arena[0].damage < arena[1].damage){
+	console.log("Player One wins!")
+	playerOne.turnPoints += 1
+}
+else if (arena[0].damage > arena[1].damage){ 
+	console.log("Player Two wins!")
+	playerTwo.turnPoints += 1
+}
+else {
+	console.log("Equal Damage! A DRAW!")
+}
+console.log("The score is currently " + playerOne.turnPoints + " to " + playerTwo.turnPoints + ".")
+
 // 		console.log who won the round
