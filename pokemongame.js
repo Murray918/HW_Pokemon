4e49cd9b0814f607cd1e31780c9d605194a4e407
ohheyitslance <ohheyitslance@ohheyitslances-MacBook.local>
2018-12-20 12:18:02 -0600

    completed round 1

--- pokemongame.js
+++ pokemongame.js
@@ -54,36 +54,49 @@ for (let i = 0; i <= 2; i++){
 // output for the hand that player two drew
 console.log(`Player Two drew ${playerTwo.hand[0].name}, ${playerTwo.hand[1].name}, ${playerTwo.hand[2].name}!`)
 
+// this is where turns 1-3 loop
+for (let i = 0; i <3; i++){
+		console.log("Turn "+ (i+1) + "!")
+	// logic to choose the highest damage card from players ones hand, and move that card into the arena
+	playerOne.hand.sort(function compareNumbers(a, b){
+	                   return b.damage - a.damage
+	               });
+	let highestCard = playerOne.hand[0];
+	arena.push(playerOne.hand.splice(0,1)[0])
 
+	// // used to randomly push a card from player two's hand into the arena
+	arena.push(playerTwo.hand.splice(Math.floor(Math.random() * playerTwo.hand.length), 1)[0]);
+	// //	console.log the chosen card	
+		 
+	// who played which cards
+	console.log(arena[0].name + " vs. " + arena[1].name);
 
-console.log(playerOne.hand)
-playerOne.hand.sort(function compareNumbers(a, b){
-                   return b.damage - a.damage
-               });
-let highestCard = playerOne.hand[0];
-arena.push(playerOne.hand.splice(0,1)[0])
-console.log(arena)
+	// 	console.log who won the turn
+	if (arena[0].damage < arena[1].damage){
+		console.log("Player One wins!")
+		playerOne.turnPoints += 1
+	}
+	else if (arena[0].damage > arena[1].damage){ 
+		console.log("Player Two wins!")
+		playerTwo.turnPoints += 1
+	}
+	else console.log("Equal Damage! A DRAW!")
+	arena.pop()
+	arena.pop()
+}
+	if (playerOne.turnPoints < playerTwo.turnPoints){
+		console.log("Round 1 belongs to Player Two!")
+		playerOne.roundsWon += 1
+	}
+	else if (playerOne.turnPoints > playerTwo.turnPoints){ 
+		console.log("Round 1 belongs to Player One!")
+		playerTwo.roundsWon += 1
+	}
+	else console.log("Equal Damage! A DRAW!")
+	playerOne.turnPoints = 0
+	playerTwo.turnPoints = 0
+	// Use this to announce the winner of the round
+	// if (playerOne.turnPoints > playerTwo.turnPoints){
+	// 	console.log("The winner of Round 1 is Player One!")}
 
-// // used to randomly push a card from player two's hand into the arena
-// arena.push(playerTwo.hand.splice(Math.floor(Math.random() * playerTwo.hand.length), 1)[0]);
-// //	console.log the chosen card
-	
-	 
-// // who played which cards
-// console.log(arena[0].name + " vs. " + arena[1].name);
-
-// // 	console.log who won the turn
-// if (arena[0].damage < arena[1].damage){
-// 	console.log("Player One wins!")
-// 	playerOne.turnPoints += 1
-// }
-// else if (arena[0].damage > arena[1].damage){ 
-// 	console.log("Player Two wins!")
-// 	playerTwo.turnPoints += 1
-// }
-// else console.log("Equal Damage! A DRAW!")
-// // Use this to announce the winner of the round
-// // if (playerOne.turnPoints > playerTwo.turnPoints){
-// // 	console.log("The winner of Round 1 is Player One!")}
-
-// // 		console.log who won the round
+	// // 		console.log who won the round
