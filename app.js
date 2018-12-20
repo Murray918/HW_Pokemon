987f09576fd5bdba69695a8f05af5f51b6d073ab
Tristan Bennett <tristanbennett@tristanova.local>
2018-12-19 22:37:59 -0600

    refactored logic for choosing playerOnes highest card to make it reusable in each turn and round

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// set up main variables
+
 let deck = [
   { name: "Bulbasaur", damage: 60 }, 
   { name: "Caterpie", damage: 40 }, 
@@ -18,6 +20,7 @@ let deck = [
   { name: "Vulpix", damage: 50 }, 
   { name: "Weedle",  damage: 40 }
 ]
+
 //create objects for each player
 let playerOne = {
 	name: "Ash",
@@ -36,8 +39,11 @@ let playerTwo = {
 //create arena to hold two players' card being played against each other
 let arena = [];
 
+
+
 //code for one round
 console.log(`Round 1 Start!`);
+
 // Remove random card from deck array and push it into playerOne.hand array
 for (let i = 0; i <= 2; i++) {
   playerOne.hand.push(deck.splice(Math.floor(Math.random() * deck.length), 1)[0]);
@@ -56,26 +62,16 @@ console.log(`Player Two drew ${playerTwo.hand[0].name}, ${playerTwo.hand[1].name
 
 // playerOne picks card
 let highestCard= [];
-if (playerOne.hand[0].damage >= playerOne.hand[1].damage) {
-  if (playerOne.hand[0].damage >= playerOne.hand[2].damage) {
-    highestCard = playerOne.hand[0]
-  } else {
-    highestCard = playerOne.hand[2]
-  }
-}
-else if (playerOne.hand[0].damage < playerOne.hand[1].damage) {
-  if (playerOne.hand[1].damage > playerOne.hand[2].damage) {
-    highestCard = playerOne.hand[1]
-  } else {
-    highestCard = playerOne.hand[2]
-  }
-}
+
+console.log(playerOne.hand.sort((a, b) => b.damage - a.damage));
+playerOne.hand.sort((a, b) => b.damage - a.damage);
+highestCard = playerOne.hand[0];
+console.log(highestCard)
+    
 arena.push(highestCard);
 
-// playerOne.hand.sort
-// .sort(function compareFunction(a, b) {
-//   return a - b;
-// })
+
+
 
 
 // playerTwo picks card
@@ -87,23 +83,22 @@ console.log(`${arena[0].name} vs. ${arena[1].name}`)
 
 //determine winner of turn
 if (arena[0].damage > arena[1].damage) {
-  console.log(`${arena[0].name} beat ${arena[1].name}. Player One wins!`)
-  playerOne.turnPoints ++
+console.log(`${arena[0].name} beat ${arena[1].name}. Player One wins!`)
+playerOne.turnPoints ++
 } else if (arena[1].damage > arena[0].dmg) {
-  console.log(`${arena[1].name} beat ${arena[0].name}. Player Two wins!`)
-  playerTwo.turnPoints ++
+console.log(`${arena[1].name} beat ${arena[0].name}. Player Two wins!`)
+playerTwo.turnPoints ++
 } else {
-  console.log(`It's a...DRAW!`)
+console.log(`It's a...DRAW!`)
 }
-
 
 // console.log who won the round
 if (playerOne.turnPoints > playerTwo.turnPoints) {
-  console.log(`Round 1 Results: Player 1 is in the lead!`)
+console.log(`Round 1 Results: Player 1 is in the lead!`)
 } else if (playerTwo.turnPoints > playerOne.turnPoints) {
-  console.log(`Round 1 Results: Player 2 is in the lead!`)
+console.log(`Round 1 Results: Player 2 is in the lead!`)
 } else {
-  console.log(`Round 1 Results: It's a draw!`)
+console.log(`Round 1 Results: It's a draw!`)
 }
 
 
