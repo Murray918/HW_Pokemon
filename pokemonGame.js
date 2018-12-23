5a23ca0b30eb5bdfb7326ce28f75686c9e8f813f
AJ <ahumphries102@gmail.com>
2018-12-23 14:09:10 -0600

    Final pokemon game

--- pokemonGame.js
+++ pokemonGame.js
@@ -1,6 +1,5 @@
 class Player {
 	constructor(name){
-		this.maxDamage = 0
 		this.name = name
 		this.hand = []
 		this.deck = []
@@ -26,13 +25,15 @@ let deck = [
   { name: "Squirtle", damage: 60 }, 
   { name: "Vulpix", damage: 50 }, 
   { name: "Inciniroar",  damage: 90 }]
-
 let player1 = new Player("player1")
 let player2 = new Player("player2")
 let randomValue = 0
-let round = 3
+let rounds = 3
+let playCards = 3
+let attackers = []
+let randomCard = Math.floor(Math.random() * 3)
 // a variable that is used to tell the game when there
-// are no more cards in the deck array to stop running a while loop
+// are no more cards in the deck array to stop running its while loop
 let deckCheck = 0
 // This function sets randomValue returns a random value between 0 and 17(18?) which is
 // the total amount of cards in the deck.
@@ -40,29 +41,10 @@ let randomHandGen = function(){
 	randomValue = Math.floor(Math.random() * deck.length)
 	return randomValue
 }
-// craftHand will push an element from the player1 deck into
-let craftHand = function(){
-	for(let i = 0; i < 3; i++){
-		player1.hand.push(player1.deck[i])
-		player2.hand.push(player2.deck[i])
-	}
-	player1.deck.pop()
-	player2.deck.pop()
-}
-let playCard = function(){
-	max = 0
-	index = 0
-	for(let i = 0; i < player1.hand.length; i++){
-		if(player1.hand[i].damage > max){
-			max = player1.hand[i].damage
-			index = i
-		}
-	}
-	player1.maxDamage = player1.hand.splice(index, 1)
-	return player1.maxDamage
-}
+
 //This loop will run until there are no more cards in the deck in array
 while (deck.length > deckCheck){
+	//this function will give us a random number between 1 and 18
 	randomHandGen()
 	//loop through the deck 18 times
 	for(let n = 0; n <= deck.length; n++){
@@ -86,114 +68,101 @@ while (deck.length > deckCheck){
 		}	
 	}
 }
-//The main function that begins the game
-let gamesBegin = function(){
-	if(round > 0){
-		let round1 = function(){
-			console.log("Round 1 Begins")
-			craftHand()
-			playCard()
+let scoop = 0
 
-			console.log("player 1 draws ", player1.hand, "and chooses ", player1.maxDamage)
-			console.log("player 2 draws ", player2.hand)
-				//checking for the highest damage value within player 1's hand and
-				// seeing if it is larger than player 2's damage value. If it is
-				// log Player 1 wins and if it is not, log player 2 wins
-				if(player1.maxDamage = player2.hand){
-					player1.points += 1
-				}
-				else if(player2.hand.damage > player1.hand.damage){
-					player2.points += 1
-				}
-				else{
-					//console.log("This hand was a tie, no points awarded")
-				}
-			
-			if (player1.points > player2.points){
-				console.log("Player 1 wins round 1")
-			}
-			else{
-				console.log("Player 2 wins round 1")
-			}
-			//After round 1 is over, empty each players hand
-			player1.hand = []
-			player2.hand = []
-		}
-		round1()
-		round -= 1
-
-		let round2 = function(){
-			console.log("Round 2 Begins")
-			craftHand()
-			for(let i = 0; i < 3; i++){
-				//checking for the highest damage value within player 1's hand and
-				// seeing if it is larger than player 2's damage value. If it is
-				// log Player 1 wins and if it is not, log player 2 wins
-				if(Math.max(null,player1.hand[i].damage) > player2.hand[i].damage){
-					player1.points += 1
-				}
-				else if(player2.hand[i].damage > player1.hand[i].damage){
-					player2.points += 1
-				}
-				else{
-					//console.log("This hand was a tie, no points awarded")
-				}
-			}
-			if (player1.points > player2.points){
-				//console.log("Player 1 wins round 1")
-			}
-			else{
-				//console.log("Player 2 wins round 1")
-			}
-		}
-		//round2()
-		round -= 1
-
-		let round3 = function(){
-			//console.log("Round 3 Begins")
-			for(let i = 0; i < 3; i++){
-
-				if(player1.deck.length && player2.deck.length > 0){
-					craftHand(i)
-				}
-			
-				//checking for the highest damage value within player 1's hand and
-				// seeing if it is larger than player 2's damage value. If it is
-				// log Player 1 wins and if it is not, log player 2 wins
-				if(Math.max(null,player1.hand[i].damage) > player2.hand[i].damage){
-					player1.points += 1
-				}
-				else if(player2.hand[i].damage > player1.hand[i].damage){
-					player2.points += 1
-				}
-				else{
-
-					//console.log("This hand was a tie, no points awarded")
-				}
-			}
-			if (player1.points > player2.points){
-				//console.log("Player 1 wins round 1")
-			}
-			else{
-				//console.log("Player 2 wins round 1")
-			}
-
-		}
-		//round3()
-		round -= 1
+// craftHand puts cards from each players deck into each players hand
+let craftHand = function(){
+	//This while loop will run until each player has 3 elements from the deck array
+	// placed in their hand array
+	while(player1.hand.length < 3 || player2.hand.length < 3){
+		//push the 0 element from player deck to the end of the players hand array
+		player1.hand.push(player1.deck[0])
+		//remove the 0 element from the deck array
+		player1.deck.splice([0],1)
+		//push the 0 element from player 2's deck to the end of the player 2's hand array
+		player2.hand.push(player2.deck[0])
+		//remove the 0 element from the deck array
+		player2.deck.splice([0], 1)
 	}
-	// console.log("Player 1 points total " + player1.points)
- 	// 	console.log("Player 2 points total " + player2.points)
+	//organize player1.hand from largest damage value to lowest
+	player1.hand.sort(function(a, b) {
+			return b.damage - a.damage;
+	});
+	//after everything within this function runs each deck goes from 9 elements, to 6, to 3 and then 0
+}
 
-	// if(round == 0 && player1.points > player2.points){
-	// 	console.log("Player 1 wins it all!")
-	// }
-	// else if (player1.points < player2.points){
-	// 	console.log("Player 2 wins it all!")
-	// }
-	// else{
-	// 	console.log("what a dumb game, it's a tie")
-	// }
+// arena moves the highest damage value in player 1's hand and 
+// a random card from player 2's hand into the attackers array
+let arena = function(){
+	//take a card from hand and place it into the attackers array
+	attackers.push(player1.hand.splice(0,1)[0])
+	//label the first object in the array as player 1's
+	attackers[0].owner = "P1"
+	console.log("player 1 chooses ", attackers[0])
+	//remove a card from player 2's hand and store in tmp 
+	let tmp = player2.hand.pop()
+	//label the object removed from player's 2 hand as player 2
+	tmp.owner = "p2"
+	//push from player 2's tmp array into the arena array as the 2nd object
+	attackers.push(tmp)
+	console.log("player 2 chooses ", attackers[1])
+
+}
+
+// fight compares the 0 and 1 element's damage values and assigns points
+let fight = function(){
+	//The 0 element always belongs to player 1 and this if condition states
+	//that if the 0 element damage is greater than the 1st element player earns a point
+	if(attackers[0].damage > attackers[1].damage){
+		console.log("******* Player 1 earns a point")
+		player1.points += 1
+	}
+	//Do the same thing as before but check of the 1st element's damage value is greater than
+	//the 0 elements damage value
+	else if(attackers[0].damage < attackers[1].damage){
+		console.log("******* Player 2 earns a point")
+		player2.points += 1
+	}
+	//If the damage values are the same the game is a tie
+	else{
+		console.log("******* tie game")
+	}
+	//Remove the attacking pokemon from the attackers array
+	attackers.splice(0,2)
+}
+
+//This function runs the game which calls all the previously
+//created functions
+let gamesBegin = function(){
+	craftHand()
+	console.log("Round 1!!!! FIGHT")
+	//While the players hands are greater than 0 they will put
+	//their pokemons from their hands into the arena and fight
+	//This will all happen 3 times for a total of 9 pokemon from
+	//each players deck.
+	while(player1.hand.length > 0 || player2.hand.length > 0){
+		arena()
+		fight()
+	}
+	craftHand()
+	console.log("Round 2!!!! FIGHT")
+	while(player1.hand.length > 0 || player2.hand.length > 0){
+		arena()
+		fight()
+	}
+
+	craftHand()
+	console.log("Round 3!!!! FIGHT")
+	while(player1.hand.length > 0 || player2.hand.length > 0){
+		arena()
+		fight()
+	}
+	if(player1.points > player2.points){
+		console.log("PLAYER 1 WINS")
+	}
+	else{
+		console.log("Player 2 WINS")
+	}
 }
 
 gamesBegin()
